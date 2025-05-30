cdf4204c023bc4a1f37176972102e4d4d5bea105
Marcos Mello <marcos.mello04@gmail.com>
2025-05-30 11:11:14 -0300

    added sub-mult-div-mean-sqrt operations

--- src/main/java/marcosmello04/github/controllers/MathController.java
+++ src/main/java/marcosmello04/github/controllers/MathController.java
@@ -16,11 +16,45 @@ public class MathController {
         return toDouble(n1) + toDouble(n2);
     }
 
+    // Subtraction - http://localhost:8080/math/sub/3/5 // How to implement subtraction
+    @RequestMapping("/sub/{n1}/{n2}")
+    public Double subtraction(@PathVariable("n1") String n1, @PathVariable("n2") String n2) throws Exception {
+        if (!isNumeric(n1) || !isNumeric(n2)) throw new UnsupportedMathOperationException("Numeric values only.");
+        return toDouble(n1) - toDouble(n2);
+    }
+
+    // Multiplication - http://localhost:8080/math/mult/3/5
+    @RequestMapping("/mult/{n1}/{n2}")
+    public Double multiplication(@PathVariable("n1") String n1, @PathVariable("n2") String n2) throws Exception {
+        if (!isNumeric(n1) || !isNumeric(n2)) throw new UnsupportedMathOperationException("Numeric values only.");
+        return toDouble(n1) * toDouble(n2);
+    }
+
+    // Division - http://localhost:8080/math/div/3/5
+    @RequestMapping("/div/{n1}/{n2}")
+    public Double division(@PathVariable("n1") String n1, @PathVariable("n2") String n2) throws Exception {
+        if (!isNumeric(n1) || !isNumeric(n2)) throw new UnsupportedMathOperationException("Numeric values only.");
+        return toDouble(n1) / toDouble(n2);
+    }
+
+    // Mean - http://localhost:8080/math/mean/3/5
+    @RequestMapping("/mean/{n1}/{n2}")
+    public Double mean(@PathVariable("n1") String n1, @PathVariable("n2") String n2) throws Exception {
+        if (!isNumeric(n1) || !isNumeric(n2)) throw new UnsupportedMathOperationException("Numeric values only.");
+        return (toDouble(n1) + toDouble(n2)) / 2;
+    }
+
+    // SquareRoot - http://localhost:8080/math/sqrt/81
+    @RequestMapping("/sqrt/{n}")
+    public Double squareRoot(@PathVariable("n") String n) throws Exception {
+        if (!isNumeric(n)) throw new UnsupportedMathOperationException("Numeric values only.");
+        return Math.sqrt(toDouble(n));
+    }
+
     private Double toDouble(String n) throws Exception {
         if (n.matches("[-+]?[0-9]*\\.?[0-9]+"))
             return Double.parseDouble(n.replace(",", "."));
         else throw new UnsupportedMathOperationException("Numeric values only.");
-            //return 0.0;
     }
 
     private boolean isNumeric(String n) {
